967b1c555618985fe38c37ec3a8655111bd1f999
kaushal8582 <kaushakumar02918@gmail.com>
2025-04-16 20:06:52 +0530

    Day 7

---
+++ Day 7/index.js
@@ -0,0 +1,47 @@
+// let var const
+// var b =12;
+// let a =12;
+// const c =34;
+
+// let  x = "kaushal";
+// let y = 34.5;
+
+// var z = 45.6;
+// var n = "nikita";
+
+
+// const m = 56.45;
+// const b= "prince";
+
+// let m = 56.45;
+// let b= "prince";
+
+// m = 4534;
+// b = "kaushal";
+
+// console.log(b);
+
+// console.log(a);
+
+// var  a = 12;
+// var b =34;
+
+// var c = a+b;
+// console.log(c);
+
+
+// api -> backend -> getProfil -> {name :"kaushal", age: 23, city: "delhi"}
+
+
+
+// conditionals
+
+let a =14;
+
+if(a>12){
+    console.log("big first");
+}else if(a>10){
+    console.log("big second");
+}else{
+    console.log("last")
+}
